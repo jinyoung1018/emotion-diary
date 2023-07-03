574a0b12baa38b3f277cdc5dc55025a4589e3837
jinyoung1018 <jywsb1816@gmail.com>
2023-07-03 18:11:24 +0900

    공통 컴포넌트 세팅

--- src/App.js
+++ src/App.js
@@ -6,17 +6,41 @@ import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader 
+        headText={"App"} 
+        leftChild={<MyButton text={"왼쪽 버튼"} onClick={()=> alert("왼쪽 클릭")}></MyButton>}
+        rightChild={<MyButton text={"오른쪽 버튼"} onClick={()=> alert("오른쪽 클릭")}></MyButton>}
+        ></MyHeader>
+
         <h2>App.js</h2>
 
-<img src={process.env.PUBLIC_URL + `/assets/emotion1.png`}></img>
-<img src={process.env.PUBLIC_URL + `/assets/emotion2.png`}></img>
-<img src={process.env.PUBLIC_URL + `/assets/emotion3.png`}></img>
-<img src={process.env.PUBLIC_URL + `/assets/emotion4.png`}></img>
-<img src={process.env.PUBLIC_URL + `/assets/emotion5.png`}></img>
+        <MyButton text={'버튼'} 
+        onClick={()=>alert("버튼클릭")}
+        type={"positive"}>    
+        </MyButton>
+
+        <MyButton text={'버튼'} 
+        onClick={()=>alert("버튼클릭")}
+        type={"negative"}>    
+        </MyButton>
+
+        <MyButton text={'버튼'} 
+        onClick={()=>alert("버튼클릭")}
+        >    
+        </MyButton>
+
+        <MyButton text={'버튼'} 
+        onClick={()=>alert("버튼클릭")}
+        type={"sfdzf"}>    
+        </MyButton>
+
 
 
         <Routes>
