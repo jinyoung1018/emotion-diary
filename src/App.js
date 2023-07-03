314494e9eaa01be64df5530cc0f559073fb118d5
jinyoung1018 <jywsb1816@gmail.com>
2023-07-03 15:22:47 +0900

    라우터 실습

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/new' element={<New></New>}></Route>
           <Route path='/edit' element={<Edit />}></Route>
-          <Route path='/diary' element={<Diary></Diary>}></Route>
+          <Route path='/diary/:id' element={<Diary></Diary>}></Route>
         </Routes>
         <RouteTest></RouteTest>
       
