8195f354bc5569494b26aa4395a9ce87ea0e002f
jinyoung1018 <jywsb1816@gmail.com>
2023-07-04 14:38:48 +0900

    onCreate

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
@@ -5,44 +6,59 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
+import { useReducer, useRef } from 'react';
 
-import MyButton from './components/MyButton';
-import MyHeader from './components/MyHeader';
+const reducer = (state,action) => {
+  let newState = [];
+  switch(action.type){
+    case 'INIT': {
+      return action.data;
+    }
+    case 'CREATE': {
+      const newItem = {
+        ...action.data
+      };
+      newState = [newItem, ...state];
+      break;
+    }
+    case 'REMOVE' : {
+      newState = state.filter((it)=> it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT' : {
+      newState = state.map((it)=>it.id === action.data.id? {...action.data}: it)
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+}
 
 function App() {
+
+  const [data,dispatch] = useReducer(reducer,[])
+
+  const dataId = useRef(0);
+
+  const onCreate = (date,content,emotion)=>{
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        data: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  }
+
   return (
+    
     <BrowserRouter>
       <div className="App">
-        <MyHeader 
-        headText={"App"} 
-        leftChild={<MyButton text={"왼쪽 버튼"} onClick={()=> alert("왼쪽 클릭")}></MyButton>}
-        rightChild={<MyButton text={"오른쪽 버튼"} onClick={()=> alert("오른쪽 클릭")}></MyButton>}
-        ></MyHeader>
-
-        <h2>App.js</h2>
-
-        <MyButton text={'버튼'} 
-        onClick={()=>alert("버튼클릭")}
-        type={"positive"}>    
-        </MyButton>
-
-        <MyButton text={'버튼'} 
-        onClick={()=>alert("버튼클릭")}
-        type={"negative"}>    
-        </MyButton>
-
-        <MyButton text={'버튼'} 
-        onClick={()=>alert("버튼클릭")}
-        >    
-        </MyButton>
-
-        <MyButton text={'버튼'} 
-        onClick={()=>alert("버튼클릭")}
-        type={"sfdzf"}>    
-        </MyButton>
-
-
-
+       
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/new' element={<New></New>}></Route>
