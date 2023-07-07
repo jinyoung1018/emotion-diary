471a78fff17369d51d9e54b74ee6cca62e319074
jinyoung1018 <jywsb1816@gmail.com>
2023-07-07 15:50:26 +0900

    버튼 스타일링

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
 
 const sortOptionList = [
     {value: "latest", name:"최신순"},
@@ -13,13 +15,14 @@ const filterOptionList = [
 
 const ControlMenu = ({value,onChange,optionList}) =>{
     return ( 
-        <select value={value} onChange={(e)=>onChange(e.target.value)}>
+        <select className="ControlMenu" value={value} onChange={(e)=>onChange(e.target.value)}>
             {optionList.map((it,idx)=><option key={idx} value={it.value}>{it.name}</option>)}
         </select>
     );
 }
 
 const DiaryList = ({diaryList}) =>{
+    const navigate = useNavigate();
 
     const [sortType, setSortType] = useState("latest");
     const [filter,setFilter] = useState("all");
@@ -53,9 +56,18 @@ const DiaryList = ({diaryList}) =>{
 
 
     return (
-        <div>
-            <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}></ControlMenu>
-            <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList}></ControlMenu>
+        <div className="DiaryList">
+
+            <div className="menu_wrapper">
+                <div className="left_col">
+                    <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}></ControlMenu>
+                    <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList}></ControlMenu>
+                </div>
+                <div className="right_col">
+                    <MyButton type={'positive'} text={'새 일기 쓰기'} onClick={()=>navigate('/new')}></MyButton>
+                </div>
+            </div>
+
             {getProcessedDiaryList().map((it) => (
                 <div key={it.id}>{it.content} {it.emotion}</div>
             ))}
