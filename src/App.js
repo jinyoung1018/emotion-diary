7ac15ac8fa82edb99cf14e23a1250f7985207827
jinyoung1018 <jywsb1816@gmail.com>
2023-07-07 15:31:46 +0900

    리스트 감점  필터링

--- src/App.js
+++ src/App.js
@@ -39,25 +39,25 @@ export const DiaryDispatchContext = React.createContext();
 const dummyData = [
   {
     id : 1,
-    emoiton: 1,
+    emotion: 1,
     content: "오늘의 일기 1번",
     date: 1688537569686,
   },
   {
     id : 2,
-    emoiton: 2,
+    emotion: 2,
     content: "오늘의 일기 2번",
     date: 1688537569687,
   },
   {
     id : 3,
-    emoiton: 3,
+    emotion: 3,
     content: "오늘의 일기 3번",
     date: 1688537569688,
   },
   {
     id : 4,
-    emoiton: 4,
+    emotion: 4,
     content: "오늘의 일기 4번",
     date: 1688537569689,
   },
--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -3,7 +3,13 @@ import { useState } from "react";
 const sortOptionList = [
     {value: "latest", name:"최신순"},
     {value: "oldest", name:"오래된 순"}
-]
+];
+
+const filterOptionList = [
+    {value: "all", name: "전부 다"},
+    {value: "good", name: "좋은 감정만"},
+    {value: "bad", name: "나쁜 감정만"},
+];
 
 const ControlMenu = ({value,onChange,optionList}) =>{
     return ( 
@@ -16,8 +22,17 @@ const ControlMenu = ({value,onChange,optionList}) =>{
 const DiaryList = ({diaryList}) =>{
 
     const [sortType, setSortType] = useState("latest");
+    const [filter,setFilter] = useState("all");
 
     const getProcessedDiaryList = () =>{
+
+        const filterCallBack = (item) => {
+            if(filter === "good"){
+                return parseInt(item.emotion) <= 3;
+            }else{
+                return parseInt(item.emotion) > 3;
+            }
+        }
 
         const compare = (a,b)=>{
             if(sortType==='latest'){
@@ -29,16 +44,20 @@ const DiaryList = ({diaryList}) =>{
         }
 
         const copyList = JSON.parse(JSON.stringify(diaryList));
-        const sortedList = copyList.sort(compare);
+
+        const filteredList = filter === "all" ? copyList : copyList.filter((it)=>filterCallBack(it));
+
+        const sortedList = filteredList.sort(compare);
         return sortedList;
-    }
+    };
 
 
     return (
         <div>
             <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}></ControlMenu>
+            <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList}></ControlMenu>
             {getProcessedDiaryList().map((it) => (
-                <div key={it.id}>{it.content}</div>
+                <div key={it.id}>{it.content} {it.emotion}</div>
             ))}
         </div>
     );
