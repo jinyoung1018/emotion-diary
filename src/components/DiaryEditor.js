e7fcebd01d303b6672ab90b959c7886a13e45ae7
jinyoung1018 <jywsb1816@gmail.com>
2023-07-09 21:58:47 +0900

    일기 입력 기능 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,5 +1,5 @@
 import {useNavigate} from "react-router-dom"
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import MyHeader from "./MyHeader"
 import MyButton from "./MyButton"
@@ -41,6 +41,8 @@ const getStringDate = (date) => {
 const DiaryEditor = () =>{
     const [date,setDate] = useState(getStringDate(new Date()));
     const [emotion,setEmotion] = useState(3);
+    const [content,setContent] = useState("");
+    const contentRef = useRef();
 
     const handleClickEmote = (emotion)=>{
         setEmotion(emotion);
@@ -71,6 +73,18 @@ const DiaryEditor = () =>{
                           ></EmotionItem>))}
                     </div>
                 </section>  
+                <section>
+                    <h4>오늘의 일기</h4>
+                    <div className="input_box text_wrapper">
+                        <textarea 
+                          placeholder="오늘은 어땠나요?"
+                          ref={contentRef} 
+                          value={content} 
+                          onChange={(e)=>setContent(e.target.value)}
+                          ></textarea>
+
+                    </div>
+                </section>
             </div>   
         </div>
     )
