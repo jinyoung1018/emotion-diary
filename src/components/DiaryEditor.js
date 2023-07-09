f687bd6a4fb9afb40c602dd159de3cc21e99439e
jinyoung1018 <jywsb1816@gmail.com>
2023-07-09 21:49:12 +0900

    감정 선택 기능 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,7 +1,80 @@
-const DiaryEditor = () =>{
-    return <div>
+import {useNavigate} from "react-router-dom"
+import { useState } from "react";
 
-    </div>
+import MyHeader from "./MyHeader"
+import MyButton from "./MyButton"
+import EmotionItem from "./EmotionItem";
+
+const emotionList = [
+    {
+        emotion_id : 1,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion1.png`,
+        emotion_descript: '완전 좋음',
+    },
+    {
+        emotion_id : 2,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion2.png`,
+        emotion_descript: '좋음',
+    },
+    {
+        emotion_id : 3,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion3.png`,
+        emotion_descript: '그럭저럭',
+    },
+    {
+        emotion_id : 4,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion4.png`,
+        emotion_descript: '나쁨',
+    },
+    {
+        emotion_id : 5,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion5.png`,
+        emotion_descript: '끔찍함',
+    }
+]
+
+const getStringDate = (date) => {
+    return date.toISOString().slice(0,10);
+};
+
+
+const DiaryEditor = () =>{
+    const [date,setDate] = useState(getStringDate(new Date()));
+    const [emotion,setEmotion] = useState(3);
+
+    const handleClickEmote = (emotion)=>{
+        setEmotion(emotion);
+    }
+
+
+    const navigate = useNavigate();
+
+    return(
+        <div className="DiaryEditor">
+            <MyHeader headText={"새 일기 쓰기"} leftChild={<MyButton text={"< 뒤로가기"} onClick={()=> navigate(-1)}></MyButton>}></MyHeader> 
+            <div>
+                <section>
+                    <h4>오늘은 언제인가요?</h4>
+                    <div className="input_box">
+                        <input className="input_date" value={date} onChange={(e) => setDate(e.target.value)} type="date"></input>
+                    </div>
+                </section>  
+                <section>
+                    <h4>오늘의 감정</h4>
+                    <div className="input_box emotion_list_wrapper">
+                        {emotionList.map((it) => (
+                        <EmotionItem 
+                          key={it.emotion_id}
+                          {...it} 
+                          onClick={handleClickEmote}
+                          isSelected={it.emotion_id === emotion}
+                          ></EmotionItem>))}
+                    </div>
+                </section>  
+            </div>   
+        </div>
+    )
+   
 };
 
 export default DiaryEditor;
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,34 +1,11 @@
-import {useNavigate} from "react-router-dom"
-
-import MyHeader from "./../components/MyHeader"
-import MyButton from "./../components/MyButton"
-import { useState } from "react";
-
-const getStringDate = (date) => {
-    return date.toISOString().slice(0,10);
-};
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () =>{
-
-    const [date,setDate] = useState(getStringDate(new Date()));
-
-
-    const navigate = useNavigate();
-
-    return(
-        <div>
-            <MyHeader headText={"새 일기 쓰기"} leftChild={<MyButton text={"< 뒤로가기"} onClick={()=> navigate(-1)}></MyButton>}></MyHeader> 
-            <div>
-                <section>
-                    <h4>오늘은 언제인가요?</h4>
-                    <div className="input-box">
-                        <input className="input-date" value={date} onChange={(e) => setDate(e.target.value)} type="date"></input>
-
-                    </div>
-                </section>    
-            </div>   
-        </div>
-    )
+   return (
+    <div>
+        <DiaryEditor></DiaryEditor>
+    </div>
+   )
 };
 
 export default New;
